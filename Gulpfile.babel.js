78bc53d7b3897a70ebf78a1ae41a851fe91a9626
Nicholas Bollweg <nick.bollweg@gmail.com>
2015-12-21 12:10:57 -0500

    more doc flow

--- Gulpfile.babel.js
+++ Gulpfile.babel.js
@@ -1,24 +1,54 @@
-import gulp from 'gulp';
-import iconfont from 'gulp-iconfont';
+import fs from "fs";
+
+import _ from "lodash";
+
+import gulp from "gulp";
+
+import consolidate from "gulp-consolidate";
+import iconfont from "gulp-iconfont";
+import jade from "gulp-jade";
+import watch from "gulp-watch";
+
+const pkg = JSON.parse(fs.readFileSync("./package.json"));
+
+const runTimestamp = Math.round(Date.now() / 1000),
+  LD = "./dist/visicons.ld.json";
 
 
-const runTimestamp = Math.round(Date.now() / 1000);
+gulp.task("fonts", () => {
+  return gulp.src(["./src/icons/*.svg"])
+    .pipe(iconfont(_.extend({
+        fontName: pkg.name,
+        timestamp: runTimestamp
+      }, pkg.iconfont)))
+    .on("glyphs", (glyphs, options) => {
+      // TODO: ensure dist
+      let stream = fs.createWriteStream(LD, {flags: "w+"});
+        stream.once('open', function(fd) {
+          stream.write(JSON.stringify({glyphs, options}, null, 2));
+          stream.end();
+        });
 
-
-gulp.task('fonts', () => {
-  return gulp.src(['src/icons/*.svg'])
-    .pipe(iconfont({
-      fontName: 'visicons', // required
-      appendUnicode: true, // recommended option
-      formats: ['ttf', 'eot', 'woff', 'woff2', 'svg'],
-      timestamp: runTimestamp,
-    }))
-    .on('glyphs', (glyphs, options) => {
-      // CSS templating, e.g.
-      console.log(glyphs, options);
+      gulp.src('./src/css/visicons.css')
+        .pipe(consolidate('lodash', _.extend({
+          fontName: pkg.name,
+          glyphs: glyphs
+        }, pkg.iconfontCss)))
+        .pipe(gulp.dest('./dist/css/'));
     })
-    .pipe(gulp.dest('dist/fonts/'));
+    .pipe(gulp.dest("./dist/fonts/"));
 });
 
 
-gulp.task('default', ['fonts']);
+gulp.task("docs", function() {
+  let font = JSON.parse(fs.readFileSync(LD));
+
+  gulp.src("./src/jade/*.jade")
+    .pipe(jade({
+      locals: {font, pkg}
+    }))
+    .pipe(gulp.dest("./dist/"))
+});
+
+
+gulp.task("default", ["fonts", "docs"]);
---
+++ README.md
@@ -0,0 +1,43 @@
+# visicons
+> nice icons for data visualization
+
+So you're building a visualization builder, and want your user
+- to be able to create linked scales in two axes
+- hover over a part of your visualization and find out what type of thing it is
+
+
+`visicons` gives you a `font-face` for icons for creating and describing
+visualizations, so can use them in buttons, menus, mouseovers, or even in-line
+with text.
+
+## Use
+> TODO: publish
+```shell
+npm install visicons
+# or
+bower install visicons
+```
+
+## Icon Roadmap
+- [ ] mark
+  - [x] x-axis
+  - [x] y-axis
+  - [ ] scale
+- [ ] layout
+  - [ ] treemap
+  - [ ] force-directed
+
+## Developing
+
+```shell
+npm install
+npm run gulp
+```
+
+To request a new icon, create a pull request that adds the new icon
+to the [Roadmap](#Icon Roadmap) above. It will probably be merged!
+
+To propose a new icon, create a pull request that implements one of the
+missing roadmap icons. New icons are assigned a Unicode code point, like
+`uEA01` so that the ordering is consistent across versions: code points will be
+handed out in the order they are requested.
